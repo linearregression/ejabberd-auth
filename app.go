b02861cf37f13334da57938a14053aa4d07904f5
Max Wayt <max@wayt.me>
2015-01-29 17:55:50 +0200

    Add config file, log, and password saving on account creation

--- app.go
+++ app.go
@@ -5,19 +5,31 @@ import (
 	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
-	"fmt"
-	"github.com/gosexy/redis"
+	"flag"
+	"github.com/go-redis/redis"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
 
+type ConfigStruct struct {
+	LogFile string `json:"log_file"`
+	Redis   struct {
+		Addr     string `json:"addr"`
+		Password string `json:"password"`
+		DB       int64  `json:"db"`
+		PoolSize int    `json:"pool_size"`
+	} `json:"redis"`
+	Keys map[string]string `json:"keys"`
+}
+
 /* https://www.ejabberd.im/files/doc/dev.html#htoc8 */
 var Redis *redis.Client
-var Public_key string
-var Private_key string
+var Config *ConfigStruct
 
 func fromEjabberd(stdin *os.File) ([]string, error) {
 
@@ -48,38 +60,71 @@ func toEjabberd(stdout *os.File, result bool) {
 	stdout.Write([]byte{0, 2, 0, 0})
 }
 
+func encrypt(message string, secret string) string {
+
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
 func Auth(username string, server string, password string) bool {
 
+	log.Println("info:", "Auth user ", username, server)
+
 	key := server + ":" + username
-	yes, err := Redis.Exists(key)
-	if err != nil || yes == false {
+	yes, err := Redis.Exists(key).Result()
+	if err != nil {
+		log.Println("error:", "Auth: ", err)
 		return false
+	}
+	if !yes {
+		log.Println("info:", "unknown user...")
+		return false
+	}
+
+	hmac_passwd, err := Redis.HGet(key, "password").Result()
+	if err != nil {
+		log.Println("error:", "Auth: ", err)
+		return false
+	}
+
+	if encrypt(username+";"+server, password) == hmac_passwd {
+		return true
 	}
 
 	params := strings.Split(password, ";")
 	if len(params) != 3 {
+		log.Println("info:", "invalid auth token, len(params) != 3")
 		return false
 	}
 	pubKey := params[1]
-	if pubKey != Public_key {
+
+	if _, ok := Config.Keys[pubKey]; !ok {
+		log.Println("info:", "invalid public key: ", pubKey)
 		return false
 	}
-	token, _ := hex.DecodeString(params[0])
+
+	private_key := Config.Keys[pubKey]
+
+	token := params[0]
 	timestamp := params[2]
 
-	hasher := hmac.New(sha256.New, []byte(Private_key))
-	hasher.Write([]byte(timestamp + ";" + username))
-	tokenExpected := hasher.Sum(nil)
+	tokenExpected := encrypt(timestamp+";"+username, private_key)
 
-	return hmac.Equal(token, tokenExpected)
+	return token == tokenExpected
 }
 
 func IsUser(username string, server string) bool {
 
+	log.Println("info:", "IsUser user ", username, server)
+
 	key := server + ":" + username
 
-	yes, err := Redis.Exists(key)
+	yes, err := Redis.Exists(key).Result()
 	if err != nil {
+
+		log.Println("error:", "IsUser: ", err)
 		return false
 	}
 
@@ -93,16 +138,20 @@ func SetPass(username string, server string, password string) bool {
 
 func TryRegister(username string, server string, password string) bool {
 
+	log.Println("info:", "TryRegister user ", username, server)
+
 	key := server + ":" + username
 
-	yes, err := Redis.Exists(key)
+	yes, err := Redis.Exists(key).Result()
 	if err != nil {
+		log.Println("error:", "TryRegister: ", err)
 		return false
 	}
 
 	if !yes {
-		_, err = Redis.Set(key, `{"username": "`+username+`"}`)
+		err = Redis.HMSet(key, "username", username, "password", encrypt(username+";"+server, password)).Err()
 		if err != nil {
+			log.Println("error:", "TryRegister: ", err)
 			return false
 		}
 		return true
@@ -121,28 +170,48 @@ func RemoveUser3(username string, server string, password string) bool {
 	return false
 }
 
+func readConfig(configFile string) {
+
+	log.Println("info:", "Using configuration file: ", configFile)
+	file, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatalln("fatal:", err)
+	}
+
+	Config = new(ConfigStruct)
+	if err := json.Unmarshal(file, Config); err != nil {
+
+		log.Fatalln("fatal:", "Fail to load config: ", err)
+	}
+}
+
 func main() {
 
-	f, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	var configFile string
+	flag.StringVar(&configFile, "config", "ejabberd-auth.conf", "Ejabberd Auth configuration file")
+	flag.Parse()
+
+	readConfig(configFile)
+
+	f, err := os.OpenFile(Config.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln("fatal:", err)
 	}
 	defer f.Close()
 
 	log.SetOutput(f)
-	log.Println("This is a test log entry")
 
 	stdin := os.Stdin
 	stdout := os.Stdout
 
-	Redis = redis.New()
-	err = Redis.Connect("172.16.23.128", 6379)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	Public_key = "ceci_est_ma_public_key"
-	Private_key = "ceci_est_ma_private_key"
+	log.Println("info:", "Connecting to redis...")
+	Redis = redis.NewClient(&redis.Options{
+		Network:  "tcp",
+		Addr:     Config.Redis.Addr,
+		Password: Config.Redis.Password,
+		DB:       Config.Redis.DB,
+		PoolSize: Config.Redis.PoolSize,
+	})
 
 	running := true
 
@@ -170,6 +239,9 @@ func main() {
 				result = RemoveUser(args[1], args[2])
 			} else if args[0] == "removeuser3" && len(args) == 4 {
 				result = RemoveUser3(args[1], args[2], args[3])
+			} else {
+				log.Println("error:", "unknown command: ", args[0])
+				continue
 			}
 			toEjabberd(stdout, result)
 		}
